334a94f98f5781eb16c05d72ae9d40b7f9025165
Anton Danilchenko <anton.danilchenko@gmail.com>
2015-11-19 01:42:55 +0200

    Add a sample project - Blog

---
+++ blog/app.py
@@ -0,0 +1,5 @@
+from rails import run
+
+
+if __name__ == '__main__':
+    run(port=8800)
---
+++ blog/controllers/blog_articles.py
@@ -0,0 +1,7 @@
+from rails.response import Response
+
+
+class BlogArticles(object):
+
+    def index(self, request):
+        return Response('Blog articles home page')
